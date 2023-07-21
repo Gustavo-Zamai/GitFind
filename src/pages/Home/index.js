d43cdfbb5190269ba21dd0f8e91ef1d27ca0c911
Gustavo-Zamai <gustavosimaozamai@gmail.com>
2023-07-20 23:13:25 -0300

    Add input and button into information component

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,22 @@
 import { Header } from '../../components/Header';
+import background from '../../assets/background.png'
+import './style.css'
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className='content'> 
+        <img src ={background} className='background' alt='Imagem do mascote do Github, como background do app'/>
+      <div className='information'>
+        <div className=''>
+          <input name='usuario' placeholder='@username' />
+          <button>
+            Buscar
+          </button>
+        </div>
+      </div>
+      </div>
     </div>
   );
 }
